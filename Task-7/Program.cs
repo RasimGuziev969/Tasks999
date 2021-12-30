bde86fa72d3bb0c7fa65f251bab893961cb62b1d
guziev <zguzievrasim@gmail.com>
2021-12-30 11:46:59 +0500

    Выполнено задание 7

---
+++ Task-7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    class Program
+    {
+        public static float f(float x)
+        {
+            if (x > 2)
+                return 2;
+            if ((x <= 2) && (x >= -2))
+                return x;
+            if (x < -2)
+                return -2;
+            return -999;
+        }
+        public static void Main(string[] args)
+        {
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine(f(x));
+        }
+    }
+}
